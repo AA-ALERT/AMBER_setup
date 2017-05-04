9883a55b3a9daba6637d83ecd432799d5c25a5b8
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-05-04 11:53:50 +0200

    Typo.

--- das5_config.sh
+++ das5_config.sh
@@ -1,6 +1,6 @@
 # DAS5
 module load hdf5_18
 module load cuda80/toolkit
-export HDF5_INCLUDE="-I${HHDFINCLUDE}"
+export HDF5_INCLUDE="-I${HDF5INCLUDE}"
 export HDF5_LDFLAGS="-L${HDF5DIR}"
 export HDF5_LIBS="-lhdf5 -lhdf5_cpp -lz"
