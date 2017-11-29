ec03b35dd915c13d4bf4bddae77d0ec175586777
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-11-29 11:23:45 +0100

    Added "compile" to the option message.

--- amber.sh
+++ amber.sh
@@ -26,11 +26,12 @@ source ${DIR}/compile.sh
 
 # Usage function
 usage () {
-  echo "Usage: ${0} <install | update | tune> <branch | scenario> <configuration_path>"
+  echo "Usage: ${0} <install | update | compile | tune> <branch | scenario> <configuration_path>"
   echo -e "\tinstall <branch>: install the specified branch of AMBER."
   echo -e "\t\tbranch: development branch to install. The default is master."
   echo -e "\tupdate <branch>: update an already existing installation of AMBER. The default branch is master."
   echo -e "\t\tbranch: development branch to update. The default is master."
+  echo -e "\tcompile: recompiles and install the current branch of AMBER."
   echo -e "\ttune scenario configuration_path: tune the AMBER modules and save the generated configuration files."
   echo -e "\t\tscenario: script containing tuning parameters and constraints."
   echo -e "\t\tconfiguration_path: directory where to save the generated configuration files."
