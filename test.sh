8abc1a7dd7161c931736ad28b4ddbc254943e107
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-05-04 14:54:39 +0200

    Grep the configuration for the right device.

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ testing() {
   # Dedispersion
   if [ "${SUBBANDING}" = true ]
   then
-    CONF="`cat ${CONFS}/dedispersion_stepone.conf`"
+    CONF="`cat ${CONFS}/dedispersion_stepone.conf | grep ${DEVICE_NAME}`"
     LOCAL=""
     if [ "`echo ${CONF} | awk -F' ' '{print $4}'`" = "1" ]
     then
@@ -14,7 +14,7 @@ testing() {
     fi
     echo -n "Testing Dedispersion (step one): "
     ${INSTALL_ROOT}/bin/DedispersionTesting -opencl_platform ${OPENCL_PLATFORM} -opencl_device ${OPENCL_DEVICE} -padding ${DEVICE_PADDING} -step_one -beams ${BEAMS} -samples ${SAMPLES} -sampling_time ${SAMPLING_TIME} -min_freq ${MIN_FREQ} -channels ${CHANNELS} -channel_bandwidth ${CHANNEL_BANDWIDTH} -zapped_channels ${CONFS}/zapped_channels.conf -subbands ${SUBBANDS} -subbanding_dms ${SUBBANDING_DMS} -subbanding_dm_first ${SUBBANDING_DM_FIRST} -subbanding_dm_step ${SUBBANDING_DM_STEP} -random ${LOCAL} -threadsD0 "`echo ${CONF} | awk -F' ' '{print $6}'`" -threadsD1 "`echo ${CONF} | awk -F' ' '{print $7}'`" -itemsD0 "`echo ${CONF} | awk -F' ' '{print $9}'`" -itemsD1 "`echo ${CONF} | awk -F' ' '{print $10}'`" -unroll "`echo ${CONF} | awk -F' ' '{print $5}'`"
-    CONF="`cat ${CONFS}/dedispersion_steptwo.conf`"
+    CONF="`cat ${CONFS}/dedispersion_steptwo.conf | grep ${DEVICE_NAME}`"
     LOCAL=""
     if [ "`echo ${CONF} | awk -F' ' '{print $4}'`" = "1" ]
     then
@@ -24,7 +24,7 @@ testing() {
     ${INSTALL_ROOT}/bin/DedispersionTesting -opencl_platform ${OPENCL_PLATFORM} -opencl_device ${OPENCL_DEVICE} -padding ${DEVICE_PADDING} -step_two -beams ${BEAMS} -synthesized_beams ${SYNTHESIZED_BEAMS} -samples ${SAMPLES} -sampling_time ${SAMPLING_TIME} -min_freq ${MIN_FREQ} -channels ${CHANNELS} -channel_bandwidth ${CHANNEL_BANDWIDTH} -subbands ${SUBBANDS} -subbanding_dms ${SUBBANDING_DMS} -dms ${DMS} -dm_first ${DM_FIRST} -dm_step ${DM_STEP} -random ${LOCAL} -threadsD0 "`echo ${CONF} | awk -F' ' '{print $6}'`" -threadsD1 "`echo ${CONF} | awk -F' ' '{print $7}'`" -itemsD0 "`echo ${CONF} | awk -F' ' '{print $9}'`" -itemsD1 "`echo ${CONF} | awk -F' ' '{print $10}'`" -unroll "`echo ${CONF} | awk -F' ' '{print $5}'`"
   else
     echo -n "Testing Dedispersion: "
-    CONF="`cat ${CONFS}/dedispersion.conf`"
+    CONF="`cat ${CONFS}/dedispersion.conf | grep ${DEVICE_NAME}`"
     LOCAL=""
     if [ "`echo ${CONF} | awk -F' ' '{print $4}'`" = "1" ]
     then
@@ -34,7 +34,7 @@ testing() {
   fi
 
   # SNR before downsampling
-  CONF="`cat ${CONFS}/snr.conf | grep " ${SAMPLES} "`"
+  CONF="`cat ${CONFS}/snr.conf | grep ${DEVICE_NAME} | grep " ${SAMPLES} "`"
   echo -n "Testing SNR for ${SAMPLES} samples: "
   if [ "${SUBBANDING}" = true ]
   then
@@ -50,20 +50,20 @@ testing() {
     if [ "${SUBBANDING}" = true ]
     then
       # Integration
-      CONF="`cat ${CONFS}/integration.conf | grep " ${STEP} "`"
+      CONF="`cat ${CONFS}/integration.conf | grep ${DEVICE_NAME} | grep " ${STEP} "`"
       echo -n "Testing Integration for step ${STEP}: "
       ${INSTALL_ROOT}/bin/IntegrationTesting -opencl_platform ${OPENCL_PLATFORM} -opencl_device ${OPENCL_DEVICE} -padding ${DEVICE_PADDING} -dms_samples -subband -integration ${STEP} -beams ${SYNTHESIZED_BEAMS} -samples ${SAMPLES} -subbanding_dms ${SUBBANDING_DMS} -dms ${DMS} -random -threadsD0 "`echo ${CONF} | awk -F' ' '{print $5}'`" -itemsD0 "`echo ${CONF} | awk -F' ' '{print $8}'`"
       # SNR after downsampling
-      CONF="`cat ${CONFS}/snr.conf | grep " ${STEP_SAMPLES} "`"
+      CONF="`cat ${CONFS}/snr.conf | grep ${DEVICE_NAME} | grep " ${STEP_SAMPLES} "`"
       echo -n "Testing SNR for ${STEP_SAMPLES} samples: "
       ${INSTALL_ROOT}/bin/SNRTesting -opencl_platform ${OPENCL_PLATFORM} -opencl_device ${OPENCL_DEVICE} -padding ${DEVICE_PADDING} -dms_samples -subband -beams ${SYNTHESIZED_BEAMS} -samples ${STEP_SAMPLES} -subbanding_dms ${SUBBANDING_DMS} -dms ${DMS} -threadsD0 "`echo ${CONF} | awk -F' ' '{print $5}'`" -itemsD0 "`echo ${CONF} | awk -F' ' '{print $8}'`"
     else
       # Integration
-      CONF="`cat ${CONFS}/integration.conf | grep " ${STEP} "`"
+      CONF="`cat ${CONFS}/integration.conf | grep ${DEVICE_NAME} | grep " ${STEP} "`"
       echo -n "Testing Integration for step ${STEP}: "
       ${INSTALL_ROOT}/bin/IntegrationTesting -opencl_platform ${OPENCL_PLATFORM} -opencl_device ${OPENCL_DEVICE} -padding ${DEVICE_PADDING} -dms_samples -integration ${STEP} -beams ${SYNTHESIZED_BEAMS} -samples ${SAMPLES} -dms ${DMS} -random -threadsD0 "`echo ${CONF} | awk -F' ' '{print $5}'`" -itemsD0 "`echo ${CONF} | awk -F' ' '{print $8}'`"
       # SNR after downsampling
-      CONF="`cat ${CONFS}/snr.conf | grep " ${STEP_SAMPLES} "`"
+      CONF="`cat ${CONFS}/snr.conf | grep ${DEVICE_NAME} | grep " ${STEP_SAMPLES} "`"
       echo -n "Testing SNR for `echo "${SAMPLES} / ${STEP}" | bc -q` samples: "
       ${INSTALL_ROOT}/bin/SNRTesting -opencl_platform ${OPENCL_PLATFORM} -opencl_device ${OPENCL_DEVICE} -padding ${DEVICE_PADDING} -dms_samples -beams ${SYNTHESIZED_BEAMS} -samples ${STEP_SAMPLES} -dms ${DMS} -threadsD0 "`echo ${CONF} | awk -F' ' '{print $5}'`" -itemsD0 "`echo ${CONF} | awk -F' ' '{print $8}'`"
     fi
