f8914245c23bf1f52373918176f40e7ace4737f0
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-11-19 16:38:21 +0100

    Proper padding.

--- tune.sh
+++ tune.sh
@@ -53,7 +53,7 @@ tune() {
       DISPERSED_SAMPLES="`echo "${SAMPLES} + (${SHIFT} * (${DM_FIRST} + ((${DMS} - 1) * ${DM_STEP})))" | bc -q`"
       DISPERSED_SAMPLES="`echo "if (${DISPERSED_SAMPLES} % 1) (${DISPERSED_SAMPLES} / 1 + 1) else (${DISPERSED_SAMPLES} / 1)" | bc -q`"
     fi
-    DISPERSED_SAMPLES="`echo "if (${DISPERSED_SAMPLES} % ${DOWNSAMPLING}) (${DISPERSED_SAMPLES} / ${DOWNSAMPLING} + 1) else (${DISPERSED_SAMPLES} / ${DOWNSAMPLING})" | bc -q`"
+    DISPERSED_SAMPLES="`echo "if (${DISPERSED_SAMPLES} % ${DOWNSAMPLING}) (${DISPERSED_SAMPLES} + (${DISPERSE_SAMPLES} % ${DOWNSAMPLING}) else (${DISPERSED_SAMPLES})" | bc -q`"
     echo -n "${DEVICE_NAME} " >> ${CONFS}/downsampling.conf
     ${INSTALL_ROOT}/bin/IntegrationTuning -iterations ${ITERATIONS} -opencl_platform ${OPENCL_PLATFORM} -opencl_device ${OPENCL_DEVICE} -padding ${DEVICE_PADDING} -vector ${DEVICE_THREADS} -min_threads ${MIN_THREADS} -max_threads ${MAX_THREADS} -max_items ${MAX_ITEMS} -in_place -before_dedispersion -integration ${DOWNSAMPLING} -beams ${BEAMS} -samples ${DISPERSED_SAMPLES} -channels ${CHANNELS} -best 2>/dev/null 1>> ${CONFS}/downsampling.conf
     SAMPLES="`echo "${SAMPLES} / ${DOWNSAMPLING}" | bc -q`"
